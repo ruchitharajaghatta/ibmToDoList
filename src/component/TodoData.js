125d739a4ba98d89dd62b2191b59d6997706c726
Ruchitha Rajaghatta <rsr262@cornell.edu>
2022-08-16 15:18:11 -0400

    updated with search capability in UI

--- src/component/TodoData.js
+++ src/component/TodoData.js
@@ -4,6 +4,22 @@ import Axios from "axios";
 
 const ShowTodos = () => {
   //Begin Here
+  const [todos, setTodos] = useState([]);
+        
+        useEffect( () => { 
+            async function fetchData() {
+                try {
+                    const res = await Axios.get('http://localhost:8080/get/items'); 
+                    setTodos(JSON.stringify(res.data));
+                    console.log(JSON.stringify(res.data));
+                } catch (err) {
+                    console.log(err);
+                }
+            }
+            fetchData();
+        }, []);
+        return <div list-style-type="circle">{todos}</div>
+
   
 }
 export default ShowTodos;
